0627dcbc827c98a69c7edbe55259a3ff29217067
QazCetelic <dev@malplena.net>
2026-02-08 20:26:16 +0100

    Add to log levels

--- src/entries/level.rs
+++ src/entries/level.rs
@@ -3,21 +3,24 @@ pub enum LogLevel {
     INFO,
     WARNING,
     ERROR,
+    FATAL,
 }
+
+pub const LOG_LEVELS: [LogLevel; 4] = [LogLevel::INFO, LogLevel::WARNING, LogLevel::ERROR, LogLevel::FATAL];
 
 impl LogLevel {
     pub fn from_str(s: &str) -> Option<LogLevel> {
         if s.eq_ignore_ascii_case("INFO") {
             Some(LogLevel::INFO)
         }
-        else if s.eq_ignore_ascii_case("WARN") {
-            Some(LogLevel::WARNING)
-        }
-        else if s.eq_ignore_ascii_case("WARNING") {
+        else if s.eq_ignore_ascii_case("WARN") || s.eq_ignore_ascii_case("WARNING") {
             Some(LogLevel::WARNING)
         }
         else if s.eq_ignore_ascii_case("ERROR") {
             Some(LogLevel::ERROR)
+        }
+        else if s.eq_ignore_ascii_case("FATAL") || s.eq_ignore_ascii_case("CRITICAL") {
+            Some(LogLevel::FATAL)
         }
         else {
             None
